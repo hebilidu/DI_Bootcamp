0fa58d67ee3a3ae9a50133c222fe0ce9ffb004c2
lidu <git@hebi.fr>
2021-04-11 14:10:53 +0300

    Day06

---
+++ Week02/Day06/Course/script.js
@@ -0,0 +1,2 @@
+alert('TEST from script file');
+console.log('Another TEST (print in console)');
---
+++ Week02/Day06/Exercises/GuessTheAnswer.js
@@ -0,0 +1,20 @@
+/* Exercicse 1 - Guess the answer */
+console.log(5 + "34"); // "534"
+console.log(5 - "4"); // error -> 1
+console.log(10 % 5); // 0
+console.log(5 % 10); // 5
+console.log("Java" + "Script"); // "JavaScript"
+console.log(" " + " "); // "  "
+console.log(" " + 0); // " 0"
+console.log(true + true); // true -> 2
+console.log(true + false); // false -> 1
+console.log(false + true); // false -> 1
+console.log(false - true); // error -> -1
+console.log(3 - 4); // -1
+console.log("Bob" - "bill"); // error -> NaN (Not a Number)
+/* Additional stuff */
+console.log(true);
+console.log(typeof(true));
+console.log(typeof(true + true));
+console.log(true == true);
+console.log(true && true);
