825822e9691dd5f41d842228560c3fc6aa6c35b0
lidu <git@hebi.fr>
2021-04-18 12:51:49 +0300

    Day10

--- Week02/Day05/XPGoldCalculator/XPGold.js
+++ Week02/Day05/XPGoldCalculator/XPGold.js
@@ -1,80 +1,63 @@
 /* *********************************************************************************** */
-console.log("\n****** Week02 Day 04 - XP Gold Exercise 1 : is_Blank ******");
-/*
-Write a program to check whether a string is blank or not.
-console.log(is_Blank('')); --> true
-console.log(is_Blank('abc')); --> false
-*/
-function is_Blank(txt) { if (txt === "") { return true } else { return false }; }
-console.log(is_Blank(''));
-console.log(is_Blank('abc'));
+console.log("\n****** Week02 Day05 - JS: Mini project - XP Gold : Calculator ******");
 
-/* *********************************************************************************** */
-console.log("\n****** Week02 Day 04 - XP Gold Exercise 2 : Abbrev_name ******");
-/*
-Write a JavaScript function to convert a string into an abbreviated form.
-console.log(abbrev_name("Robin Singh")); --> "Robin S."
-*/
-function abbrev_name(fullName) {
-    output = "";
-    for (let i = 0; i < fullName.length; i++) {
-        if (fullName[i] === " ") {
-            output += fullName[i] + fullName[i + 1] + ".";
-            break;
-        } else {
-            output += fullName[i];
-        }
+let mem1 = 0;
+let mem2 = 0;
+let txt1 = "0";
+let txt2 = "0";
+let statuses = { 0: "Entering 1st operand", 1: "+", 2: "-", 3: "*", 4: "/", 5: "=" };
+let status = 0;
+
+function addVal(txt) {
+    document.getElementById('display').value = txt;
+}
+
+function number(n) {
+    if (status === "=") { reset(); };
+    if (status === 0) {
+        if (n === '.') { txt1 += n; } else { txt1 = Number(txt1 += n).toString(); };
+        addVal(txt1);
+    } else {
+        if (n === '.') { txt2 += n; } else { txt2 = Number(txt2 += n).toString(); };
+        addVal(txt2);
     };
-    return output;
 }
-console.log(abbrev_name("Robin Singh"));
 
-/* *********************************************************************************** */
-console.log("\n****** Week02 Day 04 - XP Gold Exercise 3 : SwapCase ******");
-/*
-Write a JavaScript function which takes a string as an argument and swaps the case of each character.
-For example :
-if you input 'The Quick Brown Fox' 
-the output should be 'tHE qUICK bROWN fOX'.
-*/
-function swap(txt) {
-    let output = "";
-    for (l of txt) { if (l === l.toUpperCase()) { output += l.toLowerCase() } else { output += l.toUpperCase() }; };
-    return output;
-}
-console.log(swap('The Quick Brown Fox'));
-
-/* *********************************************************************************** */
-console.log("\n****** Week02 Day 04 - XP Gold Exercise 4 : Omnipresent value ******");
-/*
-Create a function that determines whether an argument is omnipresent for a given array.
-A value is omnipresent if it exists in every subarray inside the main array.
-To illustrate:
-[[3, 4], [8, 3, 2], [3], [9, 3], [5, 3], [4, 3]]
-// 3 exists in every element inside this array, so is omnipresent.
-Examples
-isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 1) ➞ true
-isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6) ➞ false
-*/
-function isOmnipresent(arr, ref) {
-    isIt = true;
-    for (i of arr) {
-        if (!i.includes(ref)) {
-            isIt = false;
-            break;
-        }
+function operator(op) {
+    if (status === 0) {
+        status = op;
     }
-    return isIt;
 }
-console.log(isOmnipresent([
-    [1, 1],
-    [1, 3],
-    [5, 1],
-    [6, 1]
-], 1));
-console.log(isOmnipresent([
-    [1, 1],
-    [1, 3],
-    [5, 1],
-    [6, 1]
-], 6));
+
+function equal() {
+    let result = 0;
+    switch (status) {
+        case "+":
+            result = Number(txt1) + Number(txt2);
+            break;
+        case "-":
+            result = Number(txt1) - Number(txt2);
+            break;
+        case "*":
+            result = Number(txt1) + Number(txt2);
+            break;
+        case "/":
+            result = Number(txt1) / Number(txt2);
+            break;
+    }
+    status = "=";
+    txt1 = result.toString();
+    addVal(txt1);
+}
+
+function clar() {
+    if (status === 0) { txt1 = "0"; } else { txt2 = "0"; };
+    addVal("0");
+};
+
+function reset() {
+    txt1 = "0";
+    txt2 = "0";
+    status = 0;
+    addVal("0");
+};
