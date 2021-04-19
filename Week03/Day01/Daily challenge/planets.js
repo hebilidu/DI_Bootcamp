d55c6d67c27d6268b2f9f6e24f75e293d7c78d17
lidu <git@hebi.fr>
2021-04-19 18:14:04 +0300

    Day11

--- Week03/Day01/Daily challenge/planets.js
+++ Week03/Day01/Daily challenge/planets.js
@@ -1,3 +1,23 @@
-console.log("****** Week 03 Day 01 - Daily challenge: Planets ******");
+console.log("****** Week03 Day01 - Daily challenge: Planets ******");
 /*
  */
+planetList = [
+    ["Mercury", 0],
+    ["Venus", 0],
+    ["Earth", 1],
+    ["Mars", 2],
+    ["Jupiter", 67],
+    ["Saturn", 62],
+    ["Uranus", 27],
+    ["Neptune", 14],
+];
+// Create div for each planet
+for (i in planetList) {
+    let planetdiv = document.createElement('div');
+    planetdiv.classList.add("planet", planetList[i][0]);
+    let urlimage = "url('./media/" + planetList[i][0] + ".png')";
+    planetdiv.style.backgroundImage = urlimage;
+    planetdiv.style.backgroundSize = "contain";
+    planetdiv.style.backgroundRepeat = "no-repeat";
+    document.getElementsByTagName('section')[0].appendChild(planetdiv);
+};
