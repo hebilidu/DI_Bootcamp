a57636351bfb9c4d6b401ef51d3d0158ed5fb08d
lidu <git@hebi.fr>
2021-04-19 16:09:39 +0300

    Day11

--- Week03/Day01/Course/script.js
+++ Week03/Day01/Course/script.js
@@ -1,2 +1,38 @@
+alert(`Week03 Day01 - Course notes`);
 
+// let div = document.getElementById('first_div');
 
+// let my_h2 = document.createElement('h2');
+// my_h2.innerHTML = "Hello I am Daniel";
+// div.appendChild(my_h2);
+
+// console.log("Test");
+
+// 1 
+let divnodes = document.getElementsByTagName('div');
+console.log(divnodes);
+// 1 alternative
+let divnodes2 = document.querySelector('#container');
+
+// 2
+let ulnodes = document.getElementsByTagName('ul');
+console.log(ulnodes);
+
+console.log(ulnodes[0].children[0].innerText);
+console.log(ulnodes[0].children[1].innerText);
+console.log(ulnodes[1].children[0].innerText);
+console.log(ulnodes[1].children[1].innerText);
+
+for (i = 0; i < ulnodes.length; i++) {
+    console.log("ulnodes[" + i + "] = ", ulnodes[i]);
+
+    for (j = 0; j < ulnodes[i].length; j++) {
+        console.log(i, j);
+        console.log(ulnodes[i].children[j].innerText);
+    };
+};
+// 3
+
+let list0 = document.getElementsByClassName('list')[0];
+let list1 = document.getElementsByClassName('list')[1];
+console.table([list0.textContent, list1.textContent]);
--- Week03/Day01/XP/XP.js
+++ Week03/Day01/XP/XP.js
@@ -1,7 +1,51 @@
 console.log("\n****** Week03 Day 01 - XP Exercice 1 : Change the navbar ******");
 
+// Change id
+let elem = document.getElementById("navBar");
+document.getElementById('navBar').setAttribute('id', 'socialNetworkNavigation');
+
+// Create element
+const ul = document.getElementsByTagName("ul")[0];
+const li = document.createElement("li");
+
+li.innerHTML = '<a href="#">Logout</a>';
+ul.appendChild(li);
+// Show text element content
+const firstChild = ul.firstElementChild.textContent;
+const lastChild = ul.lastElementChild.textContent;
+console.log("Text of first line item = " + firstChild);
+console.log("Text of last line item = " + lastChild);
+
 /* **************************************************************************** */
 console.log("\n****** Week03 Day 01 - XP Exercice 2 : Users ******");
 
+// Change Pete to Richard
+var items = document.getElementsByTagName('li');
+
+for (i = 0; i < items.length; i++) {
+    if (items[i].innerText === "Pete") { items[i].innerText = "Richard" };
+    if (items[i].innerText === "Sarah") { items[i].parentNode.removeChild(items[i]) };
+};
+
+// Change content of first li of each ul
+var uls = document.getElementsByClassName('list');
+const addli = document.createElement("li");
+addli.innerText = "Hey students";
+
+for (i = 0; i < uls.length; i++) {
+    uls[i].firstElementChild.innerText = "Liên";
+    let addli = document.createElement("li");
+    addli.innerText = "Hey students";
+    uls[i].appendChild(addli);
+};
+
 /* **************************************************************************** */
 console.log("\n****** Week03 Day 01 - XP Exercice 3 : Users and style ******");
+const divex3 = document.getElementById('ex3');
+divex3.style.backgroundColor = "lightblue";
+divex3.style.padding = "20px";
+
+const ulex3 = document.getElementById('ulex3');
+ulex3.firstElementChild.style.display = "none";
+ulex3.lastElementChild.style.border = "3px solid";
+document.body.style.fontSize = "x-large";
