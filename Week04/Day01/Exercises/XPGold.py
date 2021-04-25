4c419aab06677afe51662c3430e79d0342e13adc
lidu <git@hebi.fr>
2021-04-25 14:55:02 +0300

    Day16

--- Week04/Day01/Exercises/XPGold.py
+++ Week04/Day01/Exercises/XPGold.py
@@ -20,47 +20,5 @@ def main():
         else:
             print('Winter !')
 
-#
-# Ask
-# the
-# user
-# to
-# input
-# a
-# month(1
-# to
-# 12).
-# Display
-# the
-# season
-# of
-# the
-# month
-# received:
-# Spring
-# runs
-# from March (
-#
-# 3) to
-# May(5)
-# Summer
-# runs
-# from June (
-#
-# 6) to
-# August(8)
-# Autumn
-# runs
-# from September (
-#
-# 9) to
-# November(11)
-# Winter
-# runs
-# from December (
-#
-# 12) to
-# February(2)
-
 if __name__ == "__main__":
     main()
