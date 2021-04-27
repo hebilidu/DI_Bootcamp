e1fef822a24a75cb5b1dd64039a52cd6165d4e8a
lidu <git@hebi.fr>
2021-04-27 14:53:33 +0300

    Day18

---
+++ Week04/Day03/Daily challenge/CaesarCypher.py
@@ -0,0 +1,56 @@
+# Week04 Day03 - Daily Challenge - Caesar Cypher
+# Created on  : 210427 by : lidu
+# Modified on : 210427 by : lidu
+#
+# In cryptography, a Caesar cipher is one of the simplest and most widely known encryption techniques.
+# It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some
+#  fixed number of positions down the alphabet.
+
+# For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. 
+# The method is named after Julius Caesar, who used it in his private correspondence.
+
+# Create a python program that encrypts and decrypts messages with ceasar cypher, 
+# the user entries the program, and then the program asks him if he wants to encrypt or decrypt, 
+# and then execute encryption/decryption on a given message and a given shift. 
+
+sample_txt = "abcde fghij klmn opqr stuvw xyz"
+
+def is_alpha(str):
+    for l in str:
+        code = ord(l)
+        if not(64 <code < 91 or 96 < code < 123):
+            return False
+    return True
+
+def main():
+    while True:
+        input_action = input("C for Cypher / D for Decypher / S for Stop : ")
+        
+        if input_action in ["S","s"]:
+            break
+        if input_action in ["D", "d"]:
+            cypher = -1
+        elif input_action in ["C", "c"]:
+            cypher = 1
+        else:
+            continue
+
+        cypher_text = ""
+        input_txt = input("Enter a text, please (empty string stops the program): ")
+        if input_txt == "":
+            break
+
+        for letter in input_txt:
+            if is_alpha(letter):
+                if cypher == 1 and ((ord(letter) > 119) or (87 < ord(letter) < 90)):
+                    cypher_text += chr(ord(letter) - 23)
+                elif cypher == -1 and ((ord(letter) < 68) or (96 < ord(letter) < 100)):
+                    cypher_text += chr(ord(letter) + 23)
+                else:
+                    cypher_text += chr(ord(letter) + 3 * cypher)
+            else:
+                cypher_text += letter
+        print("\n" + input_txt + "\n" + cypher_text + "\n")
+
+if __name__ == "__main__":
+    main()
