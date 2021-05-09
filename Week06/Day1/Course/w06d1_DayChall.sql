6b6d065b969cb995054e4207bdb251a0c69bb63c
lidu <git@hebi.fr>
2021-05-09 15:55:52 +0300

    Day26

---
+++ Week06/Day1/Course/w06d1_DayChall.sql
@@ -0,0 +1,3 @@
+-- SELECT COUNT(*) FROM actors;
+INSERT INTO actors (first_name) VALUES ('Anthony');
+-- -> ERROR:  null value in column "last_name" of relation "actors" violates not-null constraint
